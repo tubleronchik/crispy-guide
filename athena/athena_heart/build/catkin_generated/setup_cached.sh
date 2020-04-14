e766af6f5bcee8ec93f12979a1d9ac212aea7eb9
LoSk-p <alkiz.spb@gmail.com>
2020-04-14 17:02:51 +0300

    ros

---
+++ athena/athena_heart/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alena/athena/athena_heart/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alena/athena/athena_heart/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/alena/athena/athena_heart:/opt/ros/melodic/share"
---
+++ athena/athena_heart/nodes/instagram.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('athena_heart')
+import rospy
+import time
+from instagram import Account, Media, WebAgent, Story, Location, Tag, Comment
+import numpy
+from std_msgs.msg import String
+def instagram():
+	pub = rospy.Publisher('chatter', String)
+	rospy.init_node('instagram')
+	while not rospy.is_shutdown():
+		str = "hello world %s"%rospy.get_time()
+		rospy.loginfo(str)
+		pub.publish(String(str))
+		rospy.sleep(1.0)
+if __name__ == '__main__':
+	try:
+		instagram()
+	except rospy.ROSInterruptException: pass

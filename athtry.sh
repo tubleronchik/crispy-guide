2342b1265fe2d343b11bcdc714529c8aabc89629
LoSk-p <bugrovaad@gmail.com>
2020-05-14 11:12:02 +0100

    restart rosnodes

--- athtry.sh
+++ athtry.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 sleep 5
-tmux new-session -d -s ath 'roscore'
+tmux new-session -d -s rosc 'roscore'
 tmux new-session -d -s inst 'rosrun athena insta.py'
 tmux new-session -d -s serv 'rosrun athena servo.py'
+while true
+do
+sess=$(tmux ls | wc -l)
+if [[ $sess < 3 ]]
+then
+tmux kill-server
+tmux new-session -d -s rosc 'roscore'
+tmux new-session -d -s inst 'rosrun athena insta.py'
+tmux new-session -d -s serv 'rosrun athena servo.py'
+fi
+echo $sess
+sleep 0.5
+done

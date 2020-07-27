e48645a59f44b500898af59445611e7bafefcb4d
LoSk-p <54861210+LoSk-p@users.noreply.github.com>
2020-07-27 15:03:06 +0300

    + tags and followers

--- athena_ros/nodes/insta.py
+++ athena_ros/nodes/insta.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import rospy
 import requests
+import instagram
+import re
 from std_msgs.msg import String
 import time
 from instagram import Account, Media, WebAgent, Story, Location, Tag, Comment
@@ -10,9 +12,35 @@ account = Account("mixit_ru")
 posts = agent.get_media(account, pointer=None, count=5)
 
 
+def tag(url):
+	begin = "count"
+	end = "page_info"
+	request = (requests.get(url)).text
+	beg_ind = request.find(begin)+len(begin)
+	end_ind = request.find(end)
+	count = request[beg_ind:end_ind]
+	return (int((re.search('\d+', count)).group()))	
+
+url_tag = 'https://www.instagram.com/explore/tags/home/?__a=1' #link for hashtag
+
+def followers(url):
+	begin = "count"
+	end = "followed_by_viewer"
+	request = (requests.get(url)).text
+	beg_ind = request.find(begin)+len(begin)
+	end_ind = request.find(end)
+	count = request[beg_ind:end_ind]
+	return (int((re.search('\d+', count)).group()))
+
+url_followers = 'https://www.instagram.com/who/?__a=1' #link for account
+
 def data():
     res = list(list(posts)[0])
     i = 0 #counts of posts
+    tags = []
+    tags_prev = []
+    foll = []
+    foll_prev = []
     likes = []
     likes_prev = []
     comments = []
@@ -25,11 +53,17 @@ def data():
         likes.append(media.likes_count)
         comments.append(media.comments_count)
         i += 1
+	    tags.append(tag(url_tag))
+	    foll.append(followers(url_followers))
         if i == len(res):
                 likes_prev = likes
                 likes = []
                 comments_prev = comments
                 comments = []
+		        tags_prev = tags
+	            tags = []
+		        foll_prev = foll
+		        foll = []
                 time.sleep(3)	
                 try:
                 	for i in range(len(res)):
@@ -37,8 +71,9 @@ def data():
                         	agent.update(media)
                         	likes.append(media.likes_count)
                         	comments.append(media.comments_count)
-
-                        	if likes_prev[i] < likes[i] or comments_prev[i] < comments[i]:
+				            tags.append(tag(url_tag))
+				            foll.append(followers(url_followers))
+                        	if likes_prev[i] < likes[i] or comments_prev[i] < comments[i] or tags_prev[i] < tags[i] or foll_prev[i] < foll[i]:
                                 	beats = "Beats!"
                                 	rospy.loginfo(beats)
                                 	pub.publish(beats)
@@ -49,6 +84,10 @@ def data():
                 	likes_prev = []
                 	comments = []
                 	comments_prev = []
+                    tags_prev = []
+			        tags = []
+			        foll_prev = []
+			        foll = []
                 	i = 0
 
                 except requests.HTTPError:
